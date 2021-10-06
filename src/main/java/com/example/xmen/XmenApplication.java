1a346a93321bd5c684615f70f73a6de43d243dbf
Thomas Herrera <thomasleo09.tlhg@gmail.com>
2021-10-06 15:00:43 -0500

    Initial commit with spring initializer

---
+++ src/main/java/com/example/xmen/XmenApplication.java
@@ -0,0 +1,14 @@
+
+package com.example.xmen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmenApplication.class, args);
+    }
+
+}
